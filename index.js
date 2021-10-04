996b6cf5aa612246fe5bd452857e4b33d79f1e18
kiwi28 <alexkiwi.2011@gmail.com>
2021-10-04 19:34:23 +0300

    added node version

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ app.post("/", (req, res) => {
     })
     .then((resp) => {
       console.log("de la google good ", resp);
-      // res.writeHead(302, {
-      //   Location: "http://www.google.com",
-      // });
+
       res.end();
     })
     .catch((err) => {
@@ -42,32 +40,3 @@ app.post("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-//good working-ish software
-// app.post("/", (req, res) => {
-//   console.log("before fetch log: ----", req.body);
-
-//   fetch(
-//     "https://script.google.com/macros/s/AKfycbxm-AFizE_lWexi_Kg84sqZ9iqc1-XYVrqT4skwp70O_DeEl8FzKTLm1UMjbm4oPebs/exec",
-//     {
-//       body:
-//         "name=test&phone=769814325&email=kiwi@example.com&message=rdfauhvfdahjadsfkjhkfdashjb",
-//       headers: {
-//         "Content-Type": "application/x-www-form-urlencoded",
-//       },
-//       method: "post",
-//     }
-//   )
-//     .then((res) => res.json)
-//     .then((resp) => {
-//       console.log("de la google good ", resp);
-//       res.writeHead(302, {
-//         Location: "http://www.google.com",
-//       });
-//       res.end();
-//     })
-//     .catch((err) => {
-//       console.log("de la google bad ", err);
-//       res.send(err);
-//     });
-// });
